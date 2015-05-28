1d68add540230e26765dae5827d3eca958ecfafe
fgg12 <fgg12@FGG>
2015-05-28 22:28:35 +0300

    creating database and converting project to meaven!

--- VehiclePark/src/database/DataBaseUtil.java
+++ VehiclePark/src/database/DataBaseUtil.java
@@ -5,6 +5,11 @@
  */
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,13 +23,67 @@ import contracts.Vehicle;
  * @author user
  */
 public class DataBaseUtil {
-    public static void addVehicleToBase(Vehicle vehicle, int parkingPlace, Date startTimeOfParking){
-        //Mitko, implement this method! You know what to do!
+   
+	private static Connection con = null;
+	private static Statement st = null;
+	private static ResultSet rs = null;
+	
+	private static String  url = "jdbc:postgresql://localhost/hack";
+	private static String user = "postgres";
+	private static String password = "123456";
+	private static final String createTableQuery = "CREATE TABLE IF NOT EXISTS public.parking(ParkingPlace int,LicensePlate varchar(255),Owner varchar(255),ReservedHours int,EnterTime varchar(255),VehicleType int,PricePerHour varchar(255),PriceOverTime varchar(255));";
+	private static final String INSERTQUERY1 = "INSERT INTO public.parking VALUES (";
+	private static final String INSERTQUERY2 = ");";
+	private static final String REMOVEQUERY = "DELETE FROM public.parking WHERE licenseplate = ";
+	
+	
+	private static void connect(){
+		 try {
+	            con = DriverManager.getConnection(url, user, password);
+	            st = con.createStatement();
+	            rs = st.executeQuery(createTableQuery);
+	        } catch (SQLException ex) {}
+	}
+	
+	private static void disconnect(){
+		 try {
+             if (rs != null) {
+                 rs.close();
+             }
+             if (st != null) {
+                 st.close();
+             }
+             if (con != null) {
+                 con.close();
+             }
+
+         } catch (SQLException ex) {
+         	ex.printStackTrace();
+         }
+	}
+	
+	public static void addVehicleToBase(Vehicle vehicle, int parkingPlace, Date startTimeOfParking) throws NumberFormatException, SQLException{
+        connect();
+        int vehicleTypeNumber = 0;
+        switch (vehicle.getVehicleType()) {
+		case MOTORBIKE:
+			vehicleTypeNumber = 1;
+			break;
+		case CAR:
+			vehicleTypeNumber = 2;
+			break;
+		case LORRY:
+			vehicleTypeNumber = 3;
+			break;
+		}
+		st.executeUpdate(INSERTQUERY1 + parkingPlace +",'" + vehicle.getLicensePlate() + "','" + vehicle.getOwner() + "'," + vehicle.getReservedHours() + ",'" + startTimeOfParking + "'," + vehicleTypeNumber + "," + Double.parseDouble(vehicle.getRegularRate().toString()) + "," + Double.parseDouble(vehicle.getOvertimeRate().toString()) +  INSERTQUERY2);
+        disconnect();
     }
     
-    //to be implemented
-    public static void removeVehicleFromBase(Vehicle vehicle){
-        //same shit
+    public static void removeVehicleFromBase(Vehicle vehicle) throws SQLException{
+        connect();
+        st.executeUpdate(REMOVEQUERY + "'" + vehicle.getLicensePlate() + "'");
+        disconnect();
     }
 
     //To be implemented. Download parking places and vehicles from database;
